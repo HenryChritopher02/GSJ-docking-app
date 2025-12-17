7cd29f95038c5022f0f226bbbdf76b1065c7e720
Song-Ky Nguyen-Vo <118060564+HenryChritopher02@users.noreply.github.com>
2025-12-17 11:46:03 +0700

    Update paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 APP_VERSION = "1.0.0" # Updated version
 
-BASE_GITHUB_URL_FOR_DATA = "https://raw.githubusercontent.com/HenryChritopher02/bace1/main/ensemble-docking/"
-GH_API_BASE_URL = "https://api.github.com/repos/"
-GH_OWNER = "HenryChritopher02"
-GH_REPO = "bace1"
-GH_BRANCH = "main"
-GH_ENSEMBLE_DOCKING_ROOT_PATH = "ensemble-docking"
-RECEPTOR_SUBDIR_GH = "ensemble_protein/"
-CONFIG_SUBDIR_GH = "config/"
+BASE_GITHUB_URL_FOR_DATA = "https://raw.githubusercontent.com/HenryChritopher02/GSJ/main/"
+# GH_API_BASE_URL = "https://api.github.com/repos/"
+# GH_OWNER = "HenryChritopher02"
+# GH_REPO = "GSJ"
+# GH_BRANCH = "main"
+# GH_ENSEMBLE_DOCKING_ROOT_PATH = "ensemble-docking"
+# RECEPTOR_SUBDIR_GH = "ensemble_protein/"
+# CONFIG_SUBDIR_GH = "config/"
 
 APP_ROOT = Path(".") # Assumes streamlit_app.py is in the root of your project
 ENSEMBLE_DOCKING_DIR_LOCAL = APP_ROOT / "ensemble_docking"
@@ -19,7 +19,7 @@ MK_PREPARE_LIGAND_PY_LOCAL_PATH = LIGAND_PREPROCESSING_SUBDIR_LOCAL / "mk_prepar
 VINA_SCREENING_PL_LOCAL_PATH = ENSEMBLE_DOCKING_DIR_LOCAL / "Vina_screening.pl"
 
 VINA_DIR_LOCAL = APP_ROOT / "vina"
-VINA_EXECUTABLE_NAME = "vina_1.2.5_linux_x86_64" # Ensure this matches your Vina executable
+VINA_EXECUTABLE_NAME = "vina_1.2.7_linux_x86_64" # Ensure this matches your Vina executable
 VINA_PATH_LOCAL = VINA_DIR_LOCAL / VINA_EXECUTABLE_NAME
 
 WORKSPACE_PARENT_DIR = APP_ROOT / "autodock_workspace"
@@ -30,7 +30,3 @@ LIGAND_UPLOAD_TEMP_DIR = WORKSPACE_PARENT_DIR / "uploaded_ligands_temp"
 ZIP_EXTRACT_DIR_LOCAL = WORKSPACE_PARENT_DIR / "zip_extracted_ligands"
 DOCKING_OUTPUT_DIR_LOCAL = APP_ROOT / "autodock_outputs"
 
-# Prediction Model Related URL
-BACE_TRAIN_DATA_URL = "https://raw.githubusercontent.com/HenryChritopher02/machine-learning/refs/heads/master/train_data/bace_train_lipinski_mordred.csv"
-# New for Hybrid Model (Alternatively, define in prediction_utils.py)
-HYBRID_MODEL_BASE_URL = "https://raw.githubusercontent.com/HenryChritopher02/machine-learning/master/model/gnn/regression/vina125_15/"
